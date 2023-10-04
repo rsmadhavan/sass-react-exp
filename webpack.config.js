01ded68bbb9b6ac67a36632726f06a37e1847b0c
rsmadhavan <rsmadhavan2000@gmail.com>
2023-10-05 03:50:51 +0530

    Setup react dev env
    - webpack
    - babel
    - sass
    - react

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: '/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
+  target: 'web',
+  devServer: {
+    port: '5000',
+    static: {
+      directory:path.join(__dirname, 'public')
+    },
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: ['.js','.jsx','.json'],
+  },
+  module:{
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Match js jsx files
+        exclude: /node_modules/,
+        use:{
+          loader:'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'], 
+          }
+        },
+      },
+      {
+        test: /\.scss$/, //Match .scss files
+        use: ['style-loader','css-loader','sass-loader'], // Use loaders for Sass files
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html')
+    })
+  ]
+};
